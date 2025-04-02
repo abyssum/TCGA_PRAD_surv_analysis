49ca996aeae1b675f4180a6bd3c0673f402f5146
fotakis <fotakisg@gmail.com>
2025-04-02 17:54:59 +0300

    start tracking sh scripts

---
+++ sh/copy_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set source directory and destination directory
+SOURCE_DIR="/home/fotakis/myScratch/projects/Natalie/TCGA_PRAD/data/counts_all_pats"
+DEST_DIR="/home/fotakis/myScratch/projects/Natalie/TCGA_PRAD/data/total"
