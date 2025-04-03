04e0e766adf24946bb7efe11f4b3bfc78613ffc3
fotakis <fotakisg@gmail.com>
2025-04-03 20:07:53 +0300

    create out dir

--- sh/copy_files.sh
+++ sh/copy_files.sh
@@ -3,3 +3,7 @@
 # Set source directory and destination directory
 SOURCE_DIR="/home/fotakis/myScratch/projects/Natalie/TCGA_PRAD/data/counts_all_pats"
 DEST_DIR="/home/fotakis/myScratch/projects/Natalie/TCGA_PRAD/data/total"
+
+
+# Create destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
